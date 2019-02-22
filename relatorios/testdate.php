c7fa0e2dbe6ce9ef68fe3e0ec29c2b821c1602c9
mcasarin <casarin.marcio@gmail.com>
2019-02-21 21:24:23 -0300

    atualização relatório

--- relatorios/testdate.php
+++ relatorios/testdate.php
@@ -1,6 +1,7 @@
 <?php
 include '../include/function.php';
 include '../include/connect.php';
+sessao();
 
 //Declarando variaveis
 $nome = '';
@@ -44,32 +45,46 @@ $('.datas').datepicker();
 <?php
     // Função para looping nas datas selecionadas
     // fonte: https://www.if-not-true-then-false.com/2009/php-loop-through-dates-from-date-to-date-with-strtotime-function/
-    $datainicio = '01032018';
-    $datafinal = '20022019';
-	date_default_timezone_set('UTC');
-	while (strtotime($datainicio) <= strtotime($datafinal)) {
-                echo $datainicio."<br>";
-                $sql = "SELECT Nome,Empresa,Matricula,Cartao,NColetor,Descricao,Data,Hora,Acesso FROM d".$datainicio." WHERE Matricula='4104';";
-                echo $sql;
+    $datainicio = '01-03-2018';
+    $datafinal = '20-02-2019';
+    date_default_timezone_set('UTC');
+    echo "<div class=\"table-responsive\">
+        <table class=\"table\">
+        <thead align=\"center\">
+            <th>Nome</th><th>Empresa</th><th>Matricula</th><th>Cartão</th><th>Coletor</th><th>Descrição</th><th>Data</th><th>Hora</th><th>Acesso</th>
+            </thead>
+			<tbody>";
+	//while (strtotime($datainicio) <= strtotime($datafinal)) {
+                //echo $datainicio."<br>";
+                //$dataref = str_replace('-','',$datainicio);
+                //echo $dataref."<br>";
+                $sql  = 'SELECT Nome,Empresa,Matricula,Cartao,NColetor,Descricao,Data,Hora,Acesso FROM d01032018 WHERE Matricula=\'4104\'';
+                //echo $sql."<br>";
                 $sqlexe = $conn->query($sql);
-                while($linha = $sqlexe->fetch_array(MYSQLI_ASSOC)){
-                    $nome = $linha["Nome"];
-                    $empresa = $linha["Empresa"];
-                    $matricula = $linha["Matricula"];
-                    $cartao = $linha["Cartao"];
-                    $coletor = $linha["NColetor"];
-                    $descricaocol = $linha["Descricao"];
-                    $data = $linha["Data"];
-                    $hora = $linha["Hora"];
-                    $acesso = $linha["Acesso"];
-                }
-                $datainicio = date ("dmY", strtotime("+1 day", strtotime($datainicio)));
-	}  
-
-echo "<table>
-        <th><td>Nome</td><td>Empresa</td><td>Matricula</td><td>Cartão</td><td>Coletor</td><td>Descrição</td><td>Data</td><td>Hora</td><td>Acesso</td></th>
-        <tr><td>$nome</td><td>$empresa</td><td>$matricula</td><td>$cartao</td><td>$coletor</td><td>$descricaocol</td><td>$data</td><td>$hora</td><td>$acesso</td></tr>
-    </table>";
+                
+                if($sqlexe->num_rows > 0){
+                    while($linha = $sqlexe->fetch_assoc()){
+                        $nome = $linha["Nome"];
+                        $empresa = $linha["Empresa"];
+                        $matricula = $linha["Matricula"];
+                        $cartao = $linha["Cartao"];
+                        $coletor = $linha["NColetor"];
+                        $descricaocol = $linha["Descricao"];
+                        $data = $linha["Data"];
+                        $hora = $linha["Hora"];
+                        $acesso = $linha["Acesso"];
+                        
+                    } // end while
+                } /*else {
+                echo "<div class=\"alert alert-warning fade in\" role=\"alert\" style=\"width:250px\">
+                <p><strong>Algo deu errado na inserção!</strong><br>Tente novamente...</p>
+                </div>";
+                }*/
+                echo "<tr><td>$nome</td><td>$empresa</td><td>$matricula</td><td>$cartao</td><td>$coletor</td><td>$descricaocol</td><td>$data</td><td>$hora</td><td>$acesso</td></tr>";
+                $datainicio = date ("d-m-Y", strtotime("+1 day", strtotime($datainicio)));
+	//}  
+echo "</table>";
+$conn->close();
 ?>
 </body>
 </html>
